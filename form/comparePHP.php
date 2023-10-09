662e69359ec738dc8e4daea95e742c5085ec7fd4
AlbertoF-DAW <aff0017@alu.medac.es>
2023-10-09 14:09:47 +0200

    eliminar todo

--- form/comparePHP.php
+++
@@ -1,6 +0,0 @@
-<?php
-function max3(int $num1, int $num2, int $num3): string {
-    if ($num1 == $num2 && $num2 == $num3)
-        return "Son iguales";
-    return "El máximo es : " . max($num1, $num2, $num3);
-}
